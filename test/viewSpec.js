784486184f344ed35971c1ad2a2288703be123c5
Rob Moorman <r.moorman@lukkien.com>
2014-10-03 15:47:54 +0200

    added tests for merging

--- test/viewSpec.js
+++ test/viewSpec.js
@@ -1,3 +1,75 @@
 describe('Backbone.View', function() {
-    
+    var BaseView = Backbone.View.extend({
+        events: {
+            'foo': 'render'
+        }
+    });
+
+    it('should merge the property events', function() {
+        var ExtendedView = BaseView.extend({
+            events: {
+                'bar': 'render'
+            }
+        });
+
+        var view = new ExtendedView();
+
+        expect(view.events['foo']).toBeDefined();
+        expect(view.events['bar']).toBeDefined();
+    });
+
+    it('should merge the property events over multiple parents', function() {
+        var ExtendedBaseView = BaseView.extend({
+            events: {
+                'bar': 'render'
+            }
+        });
+
+        var ExtendedView = ExtendedBaseView.extend({
+            events: {
+                'biz': 'render'
+            }
+        });
+
+        var view = new ExtendedView();
+
+        expect(view.events['foo']).toBeDefined();
+        expect(view.events['bar']).toBeDefined();
+        expect(view.events['biz']).toBeDefined();
+    });
+
+    it('should override the merged property events', function() {
+        var ExtendedView = BaseView.extend({
+            events: {
+                'foo': 'remove',
+                'bar': 'render'
+            }
+        });
+
+        var view = new ExtendedView();
+
+        expect(view.events['foo']).toEqual('remove');
+        expect(view.events['bar']).toBeDefined();
+    });
+
+    it('should also merge user defined properties', function() {
+        Backbone.inheritance.viewOptions = ['ui'];
+
+        var ExtendedBaseView = BaseView.extend({
+            ui: {
+                'foo': 'button'
+            }
+        });
+
+        var ExtendedView = ExtendedBaseView.extend({
+            ui: {
+                'bar': 'button'
+            }
+        });
+
+        var view = new ExtendedView();
+
+        expect(view.ui['foo']).toBeDefined();
+        expect(view.ui['bar']).toBeDefined();
+    });
 });
